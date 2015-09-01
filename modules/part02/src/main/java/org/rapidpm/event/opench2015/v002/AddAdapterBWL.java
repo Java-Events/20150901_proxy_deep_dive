ab8cc3bdfa2c5607f68530ea31614129cd9e40f2
Sven Ruppert <sven.ruppert@gmail.com>
2015-09-01 14:12:46 +0200

    DynamicProxy v002

---
+++ modules/part02/src/main/java/org/rapidpm/event/opench2015/v002/AddAdapterBWL.java
@@ -0,0 +1,10 @@
+package org.rapidpm.event.opench2015.v002;
+
+/**
+ * Created by svenruppert on 01.09.15.
+ */
+public class AddAdapterBWL {
+  public Integer add(final Integer a, final Integer b) {
+    return a + b + 120;
+  }
+}
